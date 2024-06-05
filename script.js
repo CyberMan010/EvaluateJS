08226aeb5bcf2872173bb55b3baeed2ea0611eb3
CyberMan010 <mohamahasoun60@gmail.com>
2024-06-05 15:15:04 +0300

    auch

---
+++ script.js
@@ -0,0 +1,63 @@
+// console.log(-5*3);
+// console.log( "JavaScript" + 50);
+// console.log( 17 % 5 );
+// console.log( 5 % 17);
+// console.log( 5/10);
+// console.log( (4 === '4'));
+// console.log( (4 != 5) );
+// console.log( (7 <= 8));
+// console.log( "Hello" + 5);
+// let x = 3
+// console.log(  Math.ceil(x) - Math.floor(x) );
+// console.log(Math.pow(x,2));
+// var y = prompt("enter number");
+// alert(y);
+
+// Q2
+
+// let y = prompt("enter the first number")
+// let x = prompt("enter the second number")
+//     if(x >= y){
+//         console.log(x)
+    
+//     }
+//     else{
+//             console.log(y)
+//     }    
+
+// Q3
+
+// let max  = Math.max(8, 5);
+// console.log("the largest number is ", max);
+
+// let y = prompt("enter the evalu")
+// let x = prompt("enter the second evalu")
+
+// let sum = (parseInt(x,10) + parseInt(y,10))
+// console.log(sum)
+
+// Q6
+
+// let number = prompt("enter number to see ")
+// switch(number){
+//     case 1:
+//         console.log("1")
+//     case 2:
+//         console.log("2")
+//     case 3:
+//         console.log("3")
+//     case 4:
+//         console.log("4")
+//     case 5:
+//         console.log("5")
+//     case 6:
+//         console.log("6")
+//     case 7:
+//         console.log("7")
+//     case 8:
+//         console.log("8")
+//     case 9:
+//         console.log("9")
+//     default:
+//         console.log("PLEASE TRY AGAIN")
+// }
